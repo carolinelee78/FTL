beb991bc4f508b3d6936f6df82e0caeb8f67177b
Caroline Lee <carolinehslee78@gmail.com>
2021-07-02 12:55:32 +0900

    Rename TIASC files

---
+++ data/TIAS.R
@@ -0,0 +1,42 @@
+########### Setup Section ########### This section downloads the necessary R packages and functions we need to run the analysis and create visuals. 
+
+# clear global environment
+
+rm(list=ls())
+
+# detach all libraries
+
+detachAllPackages <- function() {
+  basic.packages <- c("package:stats", "package:graphics", "package:grDevices", "package:utils", "package:datasets", "package:methods", "package:base")
+  package.list <- search()[ifelse(unlist(gregexpr("package:", search()))==1, TRUE, FALSE)]
+  package.list <- setdiff(package.list, basic.packages)
+  if (length(package.list)>0)  for (package in package.list) detach(package,  character.only=TRUE)
+}
+detachAllPackages()
+
+# function to load libraries
+
+pkgTest <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[,  "Package"])]
+  if (length(new.pkg)) 
+    install.packages(new.pkg,  dependencies = TRUE)
+  sapply(pkg,  require,  character.only = TRUE)
+}
+
+# here is where you load any necessary packages
+# ex: stringr
+# lapply(c("stringr"),  pkgTest)
+
+lapply(c("tidyverse"),  pkgTest)
+lapply(c("ggplot2"),  pkgTest)
+lapply(c("ggthemes"),  pkgTest)
+lapply(c("ggpubr"),  pkgTest)
+library(dplyr)
+library(tidyr)
+library(RColorBrewer)
+library(ggplot2)
+library(ggpubr)
+
+# set working directory 
+
+setwd("/Users/kdlee/Desktop/Lebowitz_Lab/FTL")
