c7cc98e3b8364b70780937f322e456ca6ea7a46c
Caroline Lee <carolinehslee78@gmail.com>
2021-08-09 16:47:38 +0900

    TIAS Update

--- data/TIAS.R
+++ data/TIAS.R
@@ -86,12 +86,12 @@ TIAS2005 <- TIAS[!is.na(TIAS$TAS05),]
 
 TIAS2005$CAT <- with(TIAS2005, ifelse(
   TA050042 == 1 & TA050043 %in% c("1", "96") & TA050595 %in% c("5", "0") & TA050631 %in% c("5", "0") & TA050127 == 3 & TA050769 < 60 & 
-    TA050712 == 0 & TA050715 == 0 & TA050716 == 0 & TA050678 %in% c("3", "5", "7", "0") & TA050785 == 0 & TA050809 == 0 & TA050793 == 0 & TA050777 == 0 & TA050825 == 0 & 
-    TA050817 == 0 & TA050798 == 0 & TA050394 %in% c("1", "2", "3", "4", "7", "8", "97", "99"), "FTL_05", ifelse(
-      TA050042 == 1 & TA050043 %in% c("1", "96") & TA050595 %in% c("5", "0") & TA050631 %in% c("5", "0") & TA050127 == 3 & TA050769 < 60 & 
-        TA050712 == 0 & TA050715 == 0 & TA050716 == 0 & TA050678 %in% c("3", "5", "7", "0") & TA050785 == 0 & TA050809 == 0 & TA050793 == 0 & TA050777 == 0 & TA050825 == 0 & 
-        TA050817 == 0 & TA050798 == 0 & TA050394 == 0 & TA050371 == 1, "FTL_05", "IAC_05")))
-
+  TA050712 == 0 & TA050715 == 0 & TA050716 == 0 & TA050678 %in% c("3", "5", "7", "0") & TA050785 == 0 & TA050809 == 0 & TA050793 == 0 & 
+  TA050777 == 0 & TA050825 == 0 & TA050817 == 0 & TA050798 == 0 & TA050394 %in% c("1", "2", "3", "4", "7", "8", "97", "99"), "FTL_05", ifelse(
+    TA050042 == 1 & TA050043 %in% c("1", "96") & TA050595 %in% c("5", "0") & TA050631 %in% c("5", "0") & TA050127 == 3 & TA050769 < 60 & 
+    TA050712 == 0 & TA050715 == 0 & TA050716 == 0 & TA050678 %in% c("3", "5", "7", "0") & TA050785 == 0 & TA050809 == 0 & TA050793 == 0 & 
+    TA050777 == 0 & TA050825 == 0 & TA050817 == 0 & TA050798 == 0 & TA050394 == 0 & TA050371 == 1 & TA050091 == 0, "FTL_05", "IAC_05")))
+    
 # Before subsetting the data to only include data for the wave of interest, we are adding IDs for each row in a new column ('ID') to consistently identify each row (participant).
 
 T05_ID <- TIAS2005$ID
@@ -103,6 +103,10 @@ FTL05_ID <- TIAS2005[TIAS2005$CAT == "FTL_05", "ID"]
 # View the IDs of participants who have been identified as FTL for the 2005 wave 
 
 print(FTL05_ID)
+
+# Count the number of participants who have been identified as FTL for the 2005 wave 
+
+length(FTL05_ID)
 
 # View the number of FTL vs. IAC participants for the 2005 wave 
 
@@ -139,11 +143,11 @@ TIAS2007 <- TIAS[!is.na(TIAS$TAS07),]
 
 TIAS2007$CAT <- with(TIAS2007, ifelse(
   TA070042 == 1 & TA070043 %in% c("1", "96") & TA070570 %in% c("5", "0") & TA070602 %in% c("5", "0") & TA070127 == 3 & TA070740 < 60 & 
-    TA070683 == 0 & TA070686 == 0 & TA070687 == 0 & TA070649 %in% c("3", "5", "7", "0") & TA070756 == 0 & TA070777 == 0 & TA070764 == 0 & TA070748 == 0 & TA070793 == 0 & 
-    TA070785 == 0 & TA070769 == 0 & TA070368 %in% c("1", "2", "3", "4", "7", "8", "97", "99"), "FTL_07", ifelse(
-      TA070042 == 1 & TA070043 %in% c("1", "96") & TA070570 %in% c("5", "0") & TA070602 %in% c("5", "0") & TA070127 == 3 & TA070740 < 60 & 
-        TA070683 == 0 & TA070686 == 0 & TA070687 == 0 & TA070649 %in% c("3", "5", "7", "0") & TA070756 == 0 & TA070777 == 0 & TA070764 == 0 & TA070748 == 0 & TA070793 == 0 & 
-        TA070785 == 0 & TA070769 == 0 & TA070368 == 0 & TA070344 == 1, "FTL_07", "IAC_07")))
+  TA070683 == 0 & TA070686 == 0 & TA070687 == 0 & TA070649 %in% c("3", "5", "7", "0") & TA070756 == 0 & TA070777 == 0 & TA070764 == 0 & TA070748 == 0 & TA070793 == 0 & 
+  TA070785 == 0 & TA070769 == 0 & TA070368 %in% c("1", "2", "3", "4", "7", "8", "97", "99"), "FTL_07", ifelse(
+      TA070042 == 1 & TA070043 %in% c("1", "96") & TA070570 %in% c("5", "0") & TA070602 %in% c("5", "0") & TA070127 == 3 & TA070740 < 60 & TA070683 == 0 & TA070686 == 0 & 
+      TA070687 == 0 & TA070649 %in% c("3", "5", "7", "0") & TA070756 == 0 & TA070777 == 0 & TA070764 == 0 & TA070748 == 0 & TA070793 == 0 &  TA070785 == 0 & TA070769 == 0 & 
+      TA070368 == 0 & TA070344 == 1 & TA070091 == 0, "FTL_07", "IAC_07")))
 
 # Before subsetting the data to only include data for the wave of interest, we are adding IDs for each row in a new column ('ID') to consistently identify each row (participant).
 
@@ -156,6 +160,10 @@ FTL07_ID <- TIAS2007[TIAS2007$CAT == "FTL_07", "ID"]
 # View the IDs of participants who have been identified as FTL for the 2007 wave 
 
 print(FTL07_ID)
+
+# Count the number of participants who have been identified as FTL for the 2007 wave 
+
+length(FTL07_ID)
 
 # View the number of FTL vs. IAC participants for the 2007 wave 
 
@@ -192,11 +200,12 @@ TIAS2009 <- TIAS[!is.na(TIAS$TAS09),]
 
 TIAS2009$CAT <- with(TIAS2009, ifelse(
   TA090043 == 1 & TA090044 %in% c("1", "96") & TA090612 %in% c("5", "0") & TA090655 %in% c("5", "0") & TA090136 == 3 & TA090799 < 60 & 
-    TA090739 == 0 & TA090742 == 0 & TA090743 == 0 & TA090705 %in% c("3", "5", "7", "0") & TA090815 == 0 & TA090836 == 0 & TA090823 == 0 & TA090807 == 0 & TA090852 == 0 & 
-    TA090844 == 0 & TA090828 == 0 & TA090385 %in% c("1", "2", "3", "4", "7", "8", "97", "99"), "FTL_09", ifelse(
-      TA090043 == 1 & TA090044 %in% c("1", "96") & TA090612 %in% c("5", "0") & TA090655 %in% c("5", "0") & TA090136 == 3 & TA090799 < 60 & 
-        TA090739 == 0 & TA090742 == 0 & TA090743 == 0 & TA090705 %in% c("3", "5", "7", "0") & TA090815 == 0 & TA090836 == 0 & TA090823 == 0 & TA090807 == 0 & TA090852 == 0 & 
-        TA090844 == 0 & TA090828 == 0 & TA090385 == 0 & TA090361 == 1, "FTL_09", "IAC_09")))
+  TA090739 == 0 & TA090742 == 0 & TA090743 == 0 & TA090705 %in% c("3", "5", "7", "0") & TA090815 == 0 & TA090836 == 0 & TA090823 == 0 & 
+  TA090807 == 0 & TA090852 == 0 & TA090844 == 0 & TA090828 == 0 & TA090385 %in% c("1", "2", "3", "4", "7", "8", "97", "99"), "FTL_09", ifelse(
+       TA090043 == 1 & TA090044 %in% c("1", "96") & TA090612 %in% c("5", "0") & TA090655 %in% c("5", "0") & TA090136 == 3 & TA090799 < 60 & 
+       TA090739 == 0 & TA090742 == 0 & TA090743 == 0 & TA090705 %in% c("3", "5", "7", "0") & TA090815 == 0 & TA090836 == 0 & TA090823 == 0 & TA090807 == 0 & TA090852 == 0 & 
+       TA090844 == 0 & TA090828 == 0 & TA090385 == 0 & TA090361 == 1 & TA090100 == 0, "FTL_09", "IAC_09")))
+      
 
 # Before subsetting the data to only include data for the wave of interest, we are adding IDs for each row in a new column ('ID') to consistently identify each row (participant).
 
@@ -209,6 +218,10 @@ FTL09_ID <- TIAS2009[TIAS2009$CAT == "FTL_09", "ID"]
 # View the IDs of participants who have been identified as FTL for the 2009 wave 
 
 print(FTL09_ID)
+
+# Count the number of participants who have been identified as FTL for the 2009 wave 
+
+length(FTL09_ID)
 
 # View the number of FTL vs. IAC participants for the 2009 wave 
 
@@ -245,12 +258,12 @@ TIAS2011 <- TIAS[!is.na(TIAS$TAS11),]
 
 TIAS2011$CAT <- with(TIAS2011, ifelse(
   TA110044 == 1 & TA110045 %in% c("1", "96") & TA110699 %in% c("5", "0") & TA110743 %in% c("5", "0") & TA110137 == 3 & TA110915 < 60 & 
-    TA110829 == 0 & TA110832 == 0 & TA110833 == 0 & TA110793 %in% c("3", "5", "7", "0") & TA110931 == 0 & TA110952 == 0 & TA110939 == 0 & TA110923 == 0 & TA110968 == 0 & 
-    TA110960 == 0 & TA110944 == 0 & TA110462 %in% c("1", "2", "3", "4", "7", "8", "97", "99"), "FTL_11", ifelse(
+  TA110829 == 0 & TA110832 == 0 & TA110833 == 0 & TA110793 %in% c("3", "5", "7", "0") & TA110931 == 0 & TA110952 == 0 & TA110939 == 0 & 
+  TA110923 == 0 & TA110968 == 0 & TA110960 == 0 & TA110944 == 0 & TA110462 %in% c("1", "2", "3", "4", "7", "8", "97", "99"), "FTL_11", ifelse(
       TA110044 == 1 & TA110045 %in% c("1", "96") & TA110699 %in% c("5", "0") & TA110743 %in% c("5", "0") & TA110137 == 3 & TA110915 < 60 & 
-        TA110829 == 0 & TA110832 == 0 & TA110833 == 0 & TA110793 %in% c("3", "5", "7", "0") & TA110931 == 0 & TA110952 == 0 & TA110939 == 0 & TA110923 == 0 & TA110968 == 0 & 
-        TA110960 == 0 & TA110944 == 0 & TA110462 == 0 & TA110351 == 1, "FTL_11", "IAC_11")))
-
+      TA110829 == 0 & TA110832 == 0 & TA110833 == 0 & TA110793 %in% c("3", "5", "7", "0") & TA110931 == 0 & TA110952 == 0 & TA110939 == 0 & 
+      TA110923 == 0 & TA110968 == 0 & TA110960 == 0 & TA110944 == 0 & TA110462 == 0 & TA110351 == 1 & TA110101 == 0, "FTL_11", "IAC_11")))
+      
 # Before subsetting the data to only include data for the wave of interest, we are adding IDs for each row in a new column ('ID') to consistently identify each row (participant).
 
 T11_ID <- TIAS2011$ID
@@ -262,6 +275,10 @@ FTL11_ID <- TIAS2011[TIAS2011$CAT == "FTL_11", "ID"]
 # View the IDs of participants who have been identified as FTL for the 2011 wave 
 
 print(FTL11_ID)
+
+# Count the number of participants who have been identified as FTL for the 2011 wave 
+
+length(FTL11_ID)
 
 # View the number of FTL vs. IAC participants for the 2011 wave 
 
@@ -298,12 +315,13 @@ TIAS2013 <- TIAS[!is.na(TIAS$TAS13),]
 
 TIAS2013$CAT <- with(TIAS2013, ifelse(
   TA130043 == 1 & TA130044 %in% c("1", "96") & TA130719 %in% c("5", "0") & TA130763 %in% c("5", "0") & TA130136 == 3 & TA130948 < 60 & 
-    TA130852 == 0 & TA130855 == 0 & TA130856 == 0 & TA130813 %in% c("3", "5", "7", "0") & TA130961 == 0 & TA130982 == 0 & TA130969 == 0 & TA130861 == 0 & TA130998 == 0 &
-    TA130990 == 0 & TA130977 == 0 & TA130482 %in% c("1", "2", "3", "4", "7", "8", "97", "99"), "FTL_13", ifelse(
-      TA130043 == 1 & TA130044 %in% c("1", "96") & TA130719 %in% c("5", "0") & TA130763 %in% c("5", "0") & TA130136 == 3 & TA130948 < 60 & 
-        TA130852 == 0 & TA130855 == 0 & TA130856 == 0 & TA130813 %in% c("3", "5", "7", "0") & TA130961 == 0 & TA130982 == 0 & TA130969 == 0 & TA130861 == 0 & TA130998 == 0 & 
-        TA130990 == 0 & TA130977 == 0 & TA130482 == 0 & TA130350 == 1, "FTL_13", "IAC_13")))
+  TA130852 == 0 & TA130855 == 0 & TA130856 == 0 & TA130813 %in% c("3", "5", "7", "0") & TA130961 == 0 & TA130982 == 0 & TA130972 == 0 & 
+  TA130956 == 0 & TA131001 == 0 & TA130993 == 0 & TA130977 == 0 & TA130482 %in% c("1", "2", "3", "4", "7", "8", "97", "99"), "FTL_13", ifelse(
+    TA130043 == 1 & TA130044 %in% c("1", "96") & TA130719 %in% c("5", "0") & TA130763 %in% c("5", "0") & TA130136 == 3 & TA130948 < 60 & 
+    TA130852 == 0 & TA130855 == 0 & TA130856 == 0 & TA130813 %in% c("3", "5", "7", "0") & TA130961 == 0 & TA130982 == 0 & TA130969 == 0 & TA130861 == 0 & TA130998 == 0 & 
+    TA130990 == 0 & TA130977 == 0 & TA130482 == 0 & TA130350 == 1 & TA130100 == 0, "FTL_13", "IAC_13")))
 
+      
 # Before subsetting the data to only include data for the wave of interest, we are adding IDs for each row in a new column ('ID') to consistently identify each row (participant).
 
 T13_ID <- TIAS2013$ID
@@ -315,6 +333,10 @@ FTL13_ID <- TIAS2013[TIAS2013$CAT == "FTL_13", "ID"]
 # View the IDs of participants who have been identified as FTL for the 2013 wave 
 
 print(FTL13_ID)
+
+# Count the number of participants who have been identified as FTL for the 2013 wave 
+
+length(FTL13_ID)
 
 # View the number of FTL vs. IAC participants for the 2013 wave 
 
@@ -351,12 +373,14 @@ TIAS2015 <- TIAS[!is.na(TIAS$TAS15),]
 
 TIAS2015$CAT <- with(TIAS2015, ifelse(
   TA150043 == 1 & TA150044 %in% c("1", "96") & TA150731 %in% c("5", "0") & TA150776 %in% c("5", "0") & TA150128 == 3 & TA150970 < 60 & 
-    TA150869 == 0 & TA150872 == 0 & TA150873 == 0 & TA150826 %in% c("3", "5", "7", "0") & TA150986 == 0 & TA151007 == 0 & TA150994 == 0 & TA150978 == 0 & TA151023 == 0 & 
-    TA151015 == 0 & TA150999 == 0 & TA150491 %in% c("1", "2", "3", "4", "7", "8", "97", "99"), "FTL_15", ifelse(
+  TA150869 == 0 & TA150872 == 0 & TA150873 == 0 & TA150826 %in% c("3", "5", "7", "0") & TA150986 == 0 & TA151007 == 0 & TA150994 == 0 & 
+  TA150978 == 0 & TA151023 == 0 & TA151015 == 0 & TA150999 == 0 & TA150491 %in% c("1", "2", "3", "4", "7", "8", "97", "99"), "FTL_15", ifelse(
       TA150043 == 1 & TA150044 %in% c("1", "96") & TA150731 %in% c("5", "0") & TA150776 %in% c("5", "0") & TA150128 == 3 & TA150970 < 60 & 
-        TA150869 == 0 & TA150872 == 0 & TA150873 == 0 & TA150826 %in% c("3", "5", "7", "0") & TA150986 == 0 & TA151007 == 0 & TA150994 == 0 & TA150978 == 0 & TA151023 == 0 & 
-        TA151015 == 0 & TA150999 == 0 & TA150491 == 0 & TA150352 == 1, "FTL_15", "IAC_15")))
+      TA150869 == 0 & TA150872 == 0 & TA150873 == 0 & TA150826 %in% c("3", "5", "7", "0") & TA150986 == 0 & TA151007 == 0 & TA150994 == 0 & 
+      TA150978 == 0 & TA151023 == 0 & TA151015 == 0 & TA150999 == 0 & TA150491 == 0 & TA150352 == 1 & TA150092 == 0, "FTL_15", "IAC_15")))
+      
 
+     
 # Before subsetting the data to only include data for the wave of interest, we are adding IDs for each row in a new column ('ID') to consistently identify each row (participant).
 
 T15_ID <- TIAS2015$ID
@@ -368,6 +392,10 @@ FTL15_ID <- TIAS2015[TIAS2015$CAT == "FTL_15", "ID"]
 # View the IDs of participants who have been identified as FTL for the 2015 wave 
 
 print(FTL15_ID)
+
+# Count the number of participants who have been identified as FTL for the 2015 wave 
+
+length(FTL15_ID)
 
 # View the number of FTL vs. IAC participants for the 2015 wave 
 
@@ -404,9 +432,9 @@ TIAS2017 <- TIAS[!is.na(TIAS$TAS17),]
 
 TIAS2017$CAT <- with(TIAS2017, ifelse(
   TA170058 == 1 & TA170059 %in% c("1", "96") & TA170790 %in% c("5", "0") & TA170416 %in% c("5", "0") & TA170183 == 3 & TA171827 < 60 & 
-    TA170909 == 0 & TA170912 == 0 & TA170913 == 0 & TA170866 %in% c("3", "5", "7", "0") & TA171869 == 0 & TA171885 == 0 & TA171835 == 0 & TA171861 == 0 & TA171877 == 0 & 
-    TA171835 == 0 & TA171840 == 0 & TA170389 == 1, "FTL_17", "IAC_17"))
-
+  TA170909 == 0 & TA170912 == 0 & TA170913 == 0 & TA170866 %in% c("3", "5", "7", "0") & TA171869 == 0 & TA171885 == 0 & TA171835 == 0 & 
+  TA171861 == 0 & TA171877 == 0 &  TA171893 == 0 & TA171840 == 0 & TA170389 == 1 & TA170176 == 0, "FTL_17", "IAC_17"))
+ 
 # Before subsetting the data to only include data for the wave of interest, we are adding IDs for each row in a new column ('ID') to consistently identify each row (participant).
 
 T17_ID <- TIAS2017$ID
@@ -414,6 +442,10 @@ T17_ID <- TIAS2017$ID
 # Extract IDs of participants who have been identified as FTL for the 2017 wave 
 
 FTL17_ID <- TIAS2017[TIAS2017$CAT == "FTL_17", "ID"]
+
+# Count the number of participants who have been identified as FTL for the 2017 wave 
+
+length(FTL17_ID)
 
 # View the IDs of participants who have been identified as FTL for the 2017 wave 
 
